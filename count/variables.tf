d8c61b06d606f2c7b6b32d95838e569f8961477a
Sandeepgompa <sandeepgompa01@gmail.com>
2025-02-19 20:03:52 +0530

    TF count

---
+++ count/variables.tf
@@ -0,0 +1,19 @@
+variable "instances" {
+    default = ["mysql", "backend", "frontend"]
+}
+
+variable "zone_id" {
+    default = "Z063704428FMXN0C1VQUF" #your zone id
+}
+
+variable "domain_name" {
+    default = "sancharlearning.xyz"
+}
+
+variable "common_tags" {
+    type = map
+    default = {
+        Project = "expense"
+        Environment = "dev"
+    }
+}
